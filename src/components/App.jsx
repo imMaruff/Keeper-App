70b48846474280278571a94918c9acb9eb4aa994
Maruf <maruf.khann931@gmail.com>
2025-03-27 17:06:40 +0530

    added more notes dynamically

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,32 @@ import React from 'react'
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Note from './Note.jsx'
+import notes from "../notes.js"
 
+function createNote(note) {
+  return (
+    <Note
+      key={note.id}
+      title={note.title}
+      content={note.content}
+    />
+  );
+}
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => {
+        return (
+          <Note
+            title={note.title}
+            content={note.content}
+          />
+        );
+      })}
       <Footer />
     </div>
-)
+  )
 
 }
 
